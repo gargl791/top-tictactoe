1bf982f28182012a5963795e27b933d2d8ec63cb
Glen Garcia <gargl791@student.otago.ac.nz>
2025-02-25 01:17:03 +1300

    complete cli implementation

---
+++ js/script.js
@@ -0,0 +1,109 @@
+function Cell() {
+    // This token will represent a player object corresponding to placed token.
+    let token = 0;
+
+    function getToken() {
+        return token;
+    };
+
+    function setToken(player) {
+        token = player; 
+    };
+
+    return {getToken, setToken};
+}
+
+function GameBoard() {  
+
+    //Creates the 2d array tic-tac-toe game board.
+    let gameBoard = [[],[],[]];
+    for(let r = 0; r < gameBoard.length; r++){
+        for(let c = 0; c < 3; c++){
+            gameBoard[r].push(Cell())
+        }
+    }
+    console.log({gameBoard});
+
+    function getGameBoard() {
+        return gameBoard;
+    };
+
+    function updateCell(player, row, column){
+        let cell = gameBoard[row][column];
+        //When the cell's token is not 0, it means the position is already taken.
+        if(cell.getToken() !== 0) {
+            return;
+        }
+        cell.setToken(player);
+    };
+
+    return {getGameBoard, updateCell};  
+    
+}
+
+function Player(name, token) {
+    
+    this.name = name;
+    this.token = token;
+
+    function getPlayerName() {
+        return this.name;
+    }
+
+    function getPlayerToken() {
+        return this.token;
+    }
+
+    function setPlayerName(name) {
+        this.name = name;
+    }
+
+    function getPlayerToken(token) {
+        this.token = token;
+    }
+
+}
+
+//This function controls the flow of the game, initializes by setting the player's names.
+function GameController() {
+
+    const board = GameBoard();
+
+    const players = [ Player("Player One", 1), Player("Player Two", 2)]
+
+    function changePlayerTurn(activePlayer){
+        return activePlayer === 1 ? 2 : 1;
+    };
+
+    //using the indexes of the 2d array from 0 - 8. summing the indexes by player token % 3 == 0 means a win
+    function checkGameStatus(activePlayer){
+        let points = 0;
+        let currentCell;
+        let currentState = gameBoard.getGameBoard();
+        for(let r=0; r < gameBoard.length; r++){
+            for(let c=0; c<gameBoard[r].length; c++){
+                
+                if(currentCell.getToken === activePlayer.getToken()) {
+                    if (r == 0) {
+                        points += c;
+                    }
+                    else {
+                        points += r * c;
+                    }
+                }
+            }
+        }
+        if(points % 3 == 0) {
+            console.log(`${activePlayer} wins`)
+        }
+
+    };
+
+    function nextGame() {
+        
+    };
+}
+
+
+
+GameBoard();
